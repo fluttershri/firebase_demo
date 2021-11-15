42e3aea39ef4c6b3b89720a70076ca34c168d4c8
fluttershri <57746859+fluttershri@users.noreply.github.com>
2021-11-15 09:49:58 +0530

    Add files via upload

---
+++ student.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Student {
+  //
+  late String name;
+  late int age;
+  late double percentage;
+
+  //
+  Student({
+    required this.name,
+    required this.age,
+    required this.percentage,
+  });
+
+  //
+  Student.fromDocumentSnapshot(DocumentSnapshot documentSnapshot) {
+    this.name = documentSnapshot['name'];
+    this.age = documentSnapshot['age'];
+    this.percentage = documentSnapshot['percentage'];
+  }
+
+  //
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = Map<String, dynamic>();
+
+    map['name'] = this.name;
+    map['age'] = this.age;
+    map['percentage'] = this.percentage;
+
+    return map;
+  }
+}
